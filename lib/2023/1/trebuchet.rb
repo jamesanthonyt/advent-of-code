dc3098c30d90f34bcb5a5283dfaf64bd96fa2b0c
James Trinder <james.t@meetcleo.com>
2023-12-06 22:16:15 +0000

    Day 1 pt 2

--- lib/2023/1/trebuchet.rb
+++ lib/2023/1/trebuchet.rb
@@ -1,5 +1,17 @@
 
 class Trebuchet
+  NUMBERS = {
+    'one' => 1,
+    'two' => 2,
+    'three' => 3,
+    'four' => 4,
+    'five' => 5,
+    'six' => 6,
+    'seven' => 7,
+    'eight' => 8,
+    'nine' => 9,
+  }
+
   def initialize(file_path)
     @file_path = file_path
   end
@@ -9,11 +21,16 @@ class Trebuchet
   end
 
   def extracted_values
-    calibrations.map { |value| value.scan(/\d/) }
+    regex = Regexp.new(NUMBERS.keys.join('|') + '|\d')
+    calibrations.map { |value| value.scan(regex) }
+  end
+
+  def values_as_integers
+    extracted_values.map { |numbers| numbers.map { |number| NUMBERS[number] || number.to_i } }
   end
 
   def calibration_values
-    extracted_values.map { |value| [value.first, value.last].join.to_i }
+    values_as_integers.map { |value| [value.first, value.last].join.to_i }
   end
 
   def result
--- test/2023/1/trebuchet_test.rb
+++ test/2023/1/trebuchet_test.rb
@@ -7,6 +7,11 @@ class TrebuchetTest < Minitest::Test
 
     assert_equal 142, Trebuchet.new(dummy_file.path).result
   end
+  def test_trebuchet_returns_correct_sum_for_alphabeticals
+    dummy_file = create_dummy_file(file_contents_2)
+
+    assert_equal 281, Trebuchet.new(dummy_file.path).result
+  end
 
   private
 
@@ -23,6 +28,18 @@ class TrebuchetTest < Minitest::Test
       pqr3stu8vwx
       a1b2c3d4e5f
       treb7uchet
+    TXT_STRING
+  end
+
+  def file_contents_2
+    <<~TXT_STRING
+      two1nine
+      eightwothree
+      abcone2threexyz
+      xtwone3four
+      4nineeightseven2
+      zoneight234
+      7pqrstsixteen
     TXT_STRING
   end
 end
